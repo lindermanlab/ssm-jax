4cdfc713b3b18e3b719722a0bd410dc00c5e8cb1
Collin Schlager <schlagercollin@gmail.com>
2021-08-07 01:21:25 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 import os
 from distutils.core import setup
 
+
 setup(name='ssm',
-      version='0.9',
+      version='0.1',
       description='Bayesian learning and inference for a variety of state space models',
       author='Scott Linderman',
       author_email='scott.linderman@stanford.edu',
       url='https://github.com/lindermanlab/ssm',
       install_requires=['cython', 'numpy', 'scipy', 'matplotlib', 'scikit-learn', 'tqdm', 
-                        'seaborn', 'jax', 'jaxlib', 'h5py', 'jupyter', 'ipywidgets',
-                        'jxf @ git+https://github.com/lindermanlab/jxf@master'],
-      packages=['ssm', 'ssm.hmm'],
+                        'seaborn', 'jax', 'jaxlib', 'h5py', 'jupyter', 'ipywidgets'],
+      packages=['ssm'],
     )
