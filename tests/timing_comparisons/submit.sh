614773569383a1cd118e7d4725b3d62314ff0b4c
Collin Schlager <schlagercollin@gmail.com>
2021-10-20 10:17:27 -0700

    push new timing results

--- tests/timing_comparisons/submit.sh
+++ tests/timing_comparisons/submit.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-# Submit both ssm-old and ssm-jax test jobs
-
-# ===Tests===
-# test_laplace_em_num_trials
-# test_lds_em_num_trials
-# test_hmm_em_num_trials
-# test_hmm_em_num_timesteps
+# Submit both ssm-old and ssm-jax test jobs for a given test name
 
 launch_test () {
-    TEST_NAME=$1
+    local TEST_NAME=$1
     echo "Launching jobs for $TEST_NAME"
     sbatch --job-name ssmold.$TEST_NAME --export=TEST_NAME=$TEST_NAME --output="logs/ssmold.$TEST_NAME.%A.log" test_old.sh 
-    sbatch --job-name ssmjax.$TEST_NAME --export=TEST_NAME=$TEST_NAME --output="logs/ssmold.$TEST_NAME.%A.log" test_jax.sh 
+    sbatch --job-name ssmjax.$TEST_NAME --export=TEST_NAME=$TEST_NAME --output="logs/ssmjax.$TEST_NAME.%A.log" test_jax.sh 
 }
 
 launch_test "test_laplace_em_num_trials"
