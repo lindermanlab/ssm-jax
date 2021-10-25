f7020ba62b96c72fee2506a51b59ba23bfdb90a7
Collin Schlager <schlagercollin@gmail.com>
2021-10-25 15:55:10 -0700

    add arhmm tests

--- tests/timing_comparisons/ssm_v0_benchmark_tests/test_hmm_em.py
+++ tests/timing_comparisons/ssm_v0_benchmark_tests/test_hmm_em.py
@@ -11,10 +11,11 @@ def sample_hmm(true_hmm, num_trials, time_bins):
         all_data.append(data)
     return all_states, all_data
 
-def hmm_fit_em_setup(num_trials=5, num_timesteps=100, latent_dim=3, emissions_dim=10, num_iters=100):
-    true_hmm = HMM(emissions_dim, latent_dim, dynamics="gaussian", emissions="gaussian")
+def hmm_fit_em_setup(num_trials=5, num_timesteps=100, latent_dim=3, emissions_dim=10,
+                     num_iters=100, emissions="gaussian"):
+    true_hmm = HMM(emissions_dim, latent_dim, dynamics="gaussian", emissions=emissions)
     states, data = sample_hmm(true_hmm, num_trials=num_trials, time_bins=num_timesteps)
-    test_hmm = HMM(emissions_dim, latent_dim, dynamics="gaussian", emissions="gaussian")
+    test_hmm = HMM(emissions_dim, latent_dim, dynamics="gaussian", emissions=emissions)
     return test_hmm, data, num_iters
 
 def hmm_fit_em(hmm, data, num_iters):
@@ -22,7 +23,7 @@ def hmm_fit_em(hmm, data, num_iters):
     return lps
 
 
-#### TESTS
+#### GAUSSIAN EM TESTS
 @pytest.mark.ssmv0
 @pytest.mark.parametrize("num_trials", range(1, 202, 100))
 def test_hmm_em_fit_num_trials(benchmark, num_trials):
@@ -48,6 +49,36 @@ def test_hmm_em_fit_latent_dim(benchmark, latent_dim):
 @pytest.mark.parametrize("emissions_dim", range(2, 13, 2))
 def test_hmm_em_fit_emissions_dim(benchmark, emissions_dim):
     setup = lambda: (hmm_fit_em_setup(emissions_dim=emissions_dim), {})
+    lp = benchmark.pedantic(hmm_fit_em, setup=setup, rounds=1)
+    assert not np.any(np.isnan(lp))
+
+#### ARHMM TESTS
+#### TESTS
+@pytest.mark.ssmv0
+@pytest.mark.parametrize("num_trials", range(1, 202, 100))
+def test_arhmm_em_fit_num_trials(benchmark, num_trials):
+    setup = lambda: (hmm_fit_em_setup(num_trials=num_trials, emissions="autoregressive"), {})
+    lp = benchmark.pedantic(hmm_fit_em, setup=setup, rounds=1)
+    assert not np.any(np.isnan(lp))
+    
+@pytest.mark.ssmv0
+@pytest.mark.parametrize("num_timesteps", range(10, 20011, 10000))
+def test_arhmm_em_fit_num_timesteps(benchmark, num_timesteps):
+    setup = lambda: (hmm_fit_em_setup(num_timesteps=num_timesteps, emissions="autoregressive"), {})
+    lp = benchmark.pedantic(hmm_fit_em, setup=setup, rounds=1)
+    assert not np.any(np.isnan(lp))
+
+@pytest.mark.ssmv0
+@pytest.mark.parametrize("latent_dim", range(2, 13, 2))
+def test_arhmm_em_fit_latent_dim(benchmark, latent_dim):
+    setup = lambda: (hmm_fit_em_setup(latent_dim=latent_dim, emissions="autoregressive"), {})
+    lp = benchmark.pedantic(hmm_fit_em, setup=setup, rounds=1)
+    assert not np.any(np.isnan(lp))
+
+@pytest.mark.ssmv0
+@pytest.mark.parametrize("emissions_dim", range(2, 13, 2))
+def test_arhmm_em_fit_emissions_dim(benchmark, emissions_dim):
+    setup = lambda: (hmm_fit_em_setup(emissions_dim=emissions_dim, emissions="autoregressive"), {})
     lp = benchmark.pedantic(hmm_fit_em, setup=setup, rounds=1)
     assert not np.any(np.isnan(lp))
 
