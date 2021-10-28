b3325eb58c59496ffa4fae672cfff8fe5c7194fd
Scott Linderman <scott.linderman@gmail.com>
2021-10-28 10:29:04 -0700

    updating ARHMM to work with new DiscreteChain posteriors

--- ssm/hmm/base.py
+++ ssm/hmm/base.py
@@ -222,6 +222,41 @@ class AutoregressiveHMM(HMM):
     def num_lags(self):
         return self._emission_distribution.covariate_dimension // self._emission_distribution.data_dimension
 
+    def log_probability(self, states, data, prev_emissions=None):
+        """
+        Computes the log joint probability of a set of states and data (observations).
+
+        .. math::
+            \log p(x, y) = \log p(x_1) + \sum_{t=1}^{T-1} \log p(x_{t+1} | x_t) + \sum_{t=1}^{T} \log p(y_t | x_t)
+
+        Args:
+            states: An array of latent states (:math:`x_{1:T}`).
+            data: An array of the observed data (:math:`y_{1:T}`).
+
+        Returns:
+            lp:
+                The joint log probability of the provided states and data.
+        """
+        if prev_emissions is None:
+            prev_emissions = np.zeros((self.num_lags, self.emission_dim))
+
+        lp = 0
+        lp += self.initial_distribution().log_prob(states[0])
+        lp += self.emissions_distribution(states[0]).log_prob(data[0], covariates=prev_emissions.ravel())
+
+        def _step(carry, args):
+            prev_state, prev_emissions, lp = carry
+            state, emission = args
+            lp += self.dynamics_distribution(prev_state).log_prob(state)
+            lp += self.emissions_distribution(state).log_prob(emission, covariates=prev_emissions.ravel())
+            new_prev_emissions = np.row_stack([prev_emissions[1:], emission])
+            return (state, new_prev_emissions, lp), None
+
+        initial_carry = (states[0], np.row_stack([prev_emissions[1:], data[0]]), lp)
+        (_, _, lp), _ = lax.scan(_step, initial_carry, (states[1:], data[1:]))
+        return lp
+
+
     def sample(self, key, num_steps: int, initial_state=None, num_samples=1, prev_emissions=None):
         """
         Sample from the joint distribution defined by the state space model.
