Components refactor seems to be working now. Changes made: - fixed a broadcasting bug in InitialCondition.m_step - Split HMM notebook into Gaussian and Poisson for easier testing - Added a GaussianARHMM model and updated notebook - Updated timing tests to work with new interfaces

ssm/distributions/discrete_chain.py
@@ -265,16 +265,19 @@ class StationaryDiscreteChain(_DiscreteChain):
         T, K = self.num_timesteps, self.num_states
         batch_shape = self.batch_shape
         flat_data = data.reshape(-1, T)
+        flat_initial_potentials = self._initial_potentials.reshape(-1, K)
         flat_unary_potentials = self._unary_potentials.reshape(-1, T, K)
         flat_pairwise_potentials = self._pairwise_potentials.reshape(-1, K, K)
 
-        def _log_prob_single(z, unary, pair):
-            lp = unary[np.arange(T), z].sum()
+        def _log_prob_single(z, initial, unary, pair):
+            lp = initial[z[0]]
+            lp += unary[np.arange(T), z].sum()
             lp += pair[z[:-1], z[1:]].sum()
             lp -= self.log_normalizer
             return lp
 
         lps = vmap(_log_prob_single)(flat_data,
+                                     flat_initial_potentials,
                                      flat_unary_potentials,
                                      flat_pairwise_potentials)
 
@@ -397,16 +400,19 @@ class NonstationaryDiscreteChain(_DiscreteChain):
         T, K = self.num_timesteps, self.num_states
         batch_shape = self.batch_shape
         flat_data = data.reshape(-1, T)
+        flat_initial_potentials = self._initial_potentials.reshape(-1, K)
         flat_unary_potentials = self._unary_potentials.reshape(-1, T, K)
         flat_pairwise_potentials = self._pairwise_potentials.reshape(-1, T-1, K, K)
 
-        def _log_prob_single(z, unary, pair):
-            lp = unary[np.arange(T), z].sum()
+        def _log_prob_single(z, initial, unary, pair):
+            lp = initial[z[0]]
+            lp += unary[np.arange(T), z].sum()
             lp += pair[np.arange(T-1), z[:-1], z[1:]].sum()
             lp -= self.log_normalizer
             return lp
 
         lps = vmap(_log_prob_single)(flat_data,
+                                     flat_initial_potentials,
                                      flat_unary_potentials,
                                      flat_pairwise_potentials)
 

ssm/hmm/base.py
@@ -42,6 +42,14 @@ class HMM(SSM):
         self._transitions = transitions
         self._emissions = emissions
 
+    @property
+    def transition_matrix(self):
+        return self._transitions.transition_matrix
+
+    @property
+    def num_states(self):
+        return self._num_states
+
     def tree_flatten(self):
         children = (self._initial_condition,
                     self._transitions,
@@ -62,9 +70,6 @@ class HMM(SSM):
     def emissions_distribution(self, state):
         return self._emissions.distribution(state)
 
-    @property
-    def transition_matrix(self):
-        return self._transitions.transition_matrix
 
     ### Methods for posterior inference
     @format_dataset
@@ -113,8 +118,9 @@ class HMM(SSM):
         if posterior is None:
             posterior = self.infer_posterior(data)
 
-        dummy_states = np.zeros(data.shape[0], dtype=int)
-        return self.log_probability(dummy_states, data) - posterior.log_prob(dummy_states)
+        # dummy_states = np.zeros(data.shape[0], dtype=int)
+        # return self.log_probability(dummy_states, data) - posterior.log_prob(dummy_states)
+        return posterior.log_normalizer
 
     ### EM: Operates on batches of data (aka datasets) and posteriors
     def m_step(self, dataset, posteriors):
@@ -172,11 +178,12 @@ class AutoregressiveHMM(HMM):
     """
     @property
     def emission_dim(self):
-        return self._emission_distribution.data_dimension
+        return self._emissions._emission_distribution.data_dimension
 
     @property
     def num_lags(self):
-        return self._emission_distribution.covariate_dimension // self._emission_distribution.data_dimension
+        dist = self._emissions._emission_distribution
+        return dist.covariate_dimension // dist.data_dimension
 
     def log_probability(self, states, data, prev_emissions=None):
         """
@@ -265,14 +272,25 @@ class AutoregressiveHMM(HMM):
 
         return states, emissions
 
-    def _log_likelihoods(self, data: Array):
+    def infer_posterior(self, data):
+        ks = np.arange(self._num_states)
+        initial_log_probs = self._initial_condition.distribution().log_prob(ks)
+        transition_log_probs = vmap(
+            lambda i: self._transitions.distribution(i).log_prob(ks)
+            )(ks)
+
+        # Compute the emission log probs
         def _compute_ll(x, y):
-            ll = self._emission_distribution.log_prob(y, covariates=x.ravel())
+            ll = self._emissions._emission_distribution.log_prob(y, covariates=x.ravel())
             new_x = np.row_stack([x[1:], y])
             return new_x, ll
 
-        _, log_likelihoods = lax.scan(_compute_ll, np.zeros((self.num_lags, self.emission_dim)), data)
+        _, emission_log_probs = lax.scan(_compute_ll, np.zeros((self.num_lags, self.emission_dim)), data)
 
         # Ignore likelihood of the first bit of data since we don't have a prefix
-        log_likelihoods = log_likelihoods.at[:self.num_lags].set(0.0)
-        return log_likelihoods
+        emission_log_probs = emission_log_probs.at[:self.num_lags].set(0.0)
+
+        return StationaryDiscreteChain(
+            initial_log_probs,
+            emission_log_probs,
+            transition_log_probs)

ssm/hmm/initial.py
@@ -43,8 +43,7 @@ class StandardInitialCondition(InitialCondition):
         num_states = self._initial_distribution.probs_parameter().shape[-1]
 
         if initial_distribution_prior is None:
-            initial_distribution_prior = \
-                tfd.Dirichlet(1.1 * np.ones((num_states, num_states)))
+            initial_distribution_prior = tfd.Dirichlet(1.1 * np.ones(num_states))
         self._initial_distribution_prior = initial_distribution_prior
 
     def tree_flatten(self):
@@ -55,7 +54,7 @@ class StandardInitialCondition(InitialCondition):
     @classmethod
     def tree_unflatten(cls, aux_data, children):
         distribution, prior = children
-        return cls(initial_distribution=distribution, 
+        return cls(initial_distribution=distribution,
                    initial_distribution_prior=prior)
 
     def distribution(self):

ssm/hmm/models.py
@@ -1,13 +1,14 @@
 import jax.numpy as np
+import jax.random as jr
 import tensorflow_probability.substrates.jax as tfp
 tfd = tfp.distributions
 
 from jax.tree_util import register_pytree_node_class
 
-from ssm.hmm.base import HMM
+from ssm.hmm.base import HMM, AutoregressiveHMM
 from ssm.hmm.initial import StandardInitialCondition
 from ssm.hmm.transitions import StationaryTransitions
-from ssm.hmm.emissions import GaussianEmissions, PoissonEmissions
+from ssm.hmm.emissions import GaussianEmissions, PoissonEmissions, AutoregressiveEmissions
 
 import warnings
 
@@ -15,7 +16,7 @@ import warnings
 class GaussianHMM(HMM):
     def __init__(self,
                  num_states,
-                 num_emission_dims,
+                 num_emission_dims=None,
                  initial_state_probs=None,
                  transition_matrix=None,
                  emission_means=None,
@@ -29,8 +30,9 @@ class GaussianHMM(HMM):
             transition_matrix = np.ones((num_states, num_states)) / num_states
 
         if emission_means is None:
-            assert seed is not None, "You must either specify the means " \
-            "or give a seed (PRNGKey) so that they can be initialized randomly"
+            assert seed is not None and num_emission_dims is not None, \
+                "You must either specify the emission_means or give a dimension and seed (PRNGKey) "\
+                "so that they can be initialized randomly."
 
             means_prior = tfd.MultivariateNormalDiag(
                 np.zeros(num_emission_dims),
@@ -38,6 +40,9 @@ class GaussianHMM(HMM):
             emission_means = means_prior.sample(seed=seed, sample_shape=num_states)
 
         if emission_covariances is None:
+            assert num_emission_dims is not None, \
+                "You must either specify the emission_covariances or give a dimension "\
+                "so that they can be initialized."
             emission_covariances = np.tile(np.eye(num_emission_dims), (num_states, 1, 1))
 
         initial_condition = StandardInitialCondition(initial_probs=initial_state_probs)
@@ -67,10 +72,10 @@ class GaussianHMM(HMM):
 class PoissonHMM(HMM):
     def __init__(self,
                  num_states,
-                 num_emission_dims,
+                 num_emission_dims=None,
                  initial_state_probs=None,
                  transition_matrix=None,
-                 emission_means=None,
+                 emission_rates=None,
                  seed=None):
 
         if initial_state_probs is None:
@@ -79,18 +84,20 @@ class PoissonHMM(HMM):
         if transition_matrix is None:
             transition_matrix = np.ones((num_states, num_states)) / num_states
 
-        if emission_means is None:
-            assert seed is not None, "You must either specify the means " \
-            "or give a seed (PRNGKey) so that they can be initialized randomly"
+        if emission_rates is None:
+            assert seed is not None and num_emission_dims is not None, \
+                "You must either specify the emission_rates " \
+                "or give the num_emission_dims and a seed (PRNGKey) "\
+                "so that they can be initialized randomly"
 
             means_prior = tfp.distributions.Gamma(3.0, 1.0)
             with warnings.catch_warnings():
                 warnings.filterwarnings("ignore")
-                emission_means = means_prior.sample(seed=seed, sample_shape=(num_states, num_emission_dims))
+                emission_rates = means_prior.sample(seed=seed, sample_shape=(num_states, num_emission_dims))
 
         initial_condition = StandardInitialCondition(initial_probs=initial_state_probs)
         transitions = StationaryTransitions(transition_matrix=transition_matrix)
-        emissions = PoissonEmissions(rates=emission_means)
+        emissions = PoissonEmissions(rates=emission_rates)
         super(PoissonHMM, self).__init__(num_states,
                                          initial_condition,
                                          transitions,
@@ -110,3 +117,81 @@ class PoissonHMM(HMM):
         super(cls, obj).__init__(aux_data, *children)
         return obj
 
+@register_pytree_node_class
+class GaussianARHMM(AutoregressiveHMM):
+    def __init__(self,
+                 num_states,
+                 num_emission_dims=None,
+                 num_lags=None,
+                 initial_state_probs=None,
+                 transition_matrix=None,
+                 emission_weights=None,
+                 emission_biases=None,
+                 emission_covariances=None,
+                 seed=None):
+
+        if initial_state_probs is None:
+            initial_state_probs = np.ones(num_states) / num_states
+
+        if transition_matrix is None:
+            transition_matrix = np.ones((num_states, num_states)) / num_states
+
+        if emission_weights is None:
+            assert seed is not None and num_emission_dims is not None and num_lags is not None, \
+                "You must either specify the emission_weights or give a dimension, "\
+                "number of lags, and seed (PRNGKey) so that they can be initialized randomly."
+            this_seed, seed = jr.split(seed, 2)
+            emission_weights = tfd.Normal(0, 1).sample(
+                seed=this_seed,
+                sample_shape=(num_states, num_emission_dims, num_emission_dims * num_lags))
+
+        if emission_biases is None:
+            assert seed is not None and num_emission_dims is not None, \
+                "You must either specify the emission_weights or give a dimension, "\
+                "number of lags, and seed (PRNGKey) so that they can be initialized randomly."
+            this_seed, seed = jr.split(seed, 2)
+            emission_biases = tfd.Normal(0, 1).sample(
+                seed=this_seed,
+                sample_shape=(num_states, num_emission_dims))
+
+        if emission_covariances is None:
+            assert num_emission_dims is not None, \
+                "You must either specify the emission_covariances or give a dimension "\
+                "so that they can be initialized."
+            emission_covariances = np.tile(np.eye(num_emission_dims), (num_states, 1, 1))
+
+        initial_condition = StandardInitialCondition(initial_probs=initial_state_probs)
+        transitions = StationaryTransitions(transition_matrix=transition_matrix)
+        emissions = AutoregressiveEmissions(weights=emission_weights,
+                                            biases=emission_biases,
+                                            covariances=emission_covariances)
+        super(GaussianARHMM, self).__init__(num_states,
+                                            initial_condition,
+                                            transitions,
+                                            emissions)
+
+    @property
+    def emission_weights(self):
+        return self._emissions._emission_distribution.weights
+
+    @property
+    def emission_biases(self):
+        return self._emissions._emission_distribution.bias
+
+    @property
+    def emission_covariances(self):
+        return self._emissions._emission_distribution.scale
+
+    def tree_flatten(self):
+        children = (self._initial_condition,
+                    self._transitions,
+                    self._emissions)
+        aux_data = self._num_states
+        return children, aux_data
+
+    # directly GaussianHMM using parent (HMM) constructor
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        obj = object.__new__(cls)
+        super(cls, obj).__init__(aux_data, *children)
+        return obj