b01dae847754182b18aa88f1e8e7d6e903d6047c
Collin Schlager <schlagercollin@gmail.com>
2021-11-17 19:25:28 -0800

    reconfigure pytest to make it more accessible
    * timing-test-related logic moved to timing_comparisons dir

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,10 @@
 import pytest
+from jax.interpreters import xla
 
-### PyTest Benchmark Hooks ###
-@pytest.mark.hookwrapper
-def pytest_benchmark_generate_json(config, benchmarks, include_data, machine_info, commit_info):
-    """Disables pytest-benchmark from stripping benchmark results from errored tests.
-    See: https://pytest-benchmark.readthedocs.io/en/latest/hooks.html.
-    """
-    for bench in benchmarks:
-        if bench.has_error or len(bench.stats.data) == 0:
-            bench.stats.data = [float("nan")]
-        bench.extra_info["has_error"] = bench.has_error  # record errored state here
-        bench.fixture.has_error = False  # always set to false so it always outputs in report
-    yield
-
-# def pytest_benchmark_update_json(config, benchmarks, output_json):
-#     output_json["foo"] = "bar"
+@pytest.fixture(autouse=True)
+def cleanup():
+    """Clears XLA cache after every test."""
+    yield  # run the test
+    # clear XLA cache to prevent OOM
+    print("\nclearing XLA cache")
+    xla._xla_callable.cache_clear()
--- tests/lds/test_lds.py
+++ tests/lds/test_lds.py
@@ -35,3 +35,33 @@ def test_poisson_lds_sample():
     states, data = lds.sample(rng2, num_steps=10, num_samples=32)
     assert states.shape == (32, 10, 3)
     assert data.shape == (32, 10, 5)
+    
+def test_gaussian_lds_em_fit():
+    rng1, rng2, rng3 = jr.split(SEED, 3)
+    true_lds = GaussianLDS(3, 5, seed=rng1)
+    states, data = true_lds.sample(rng2, num_steps=100, num_samples=3)
+    test_lds = GaussianLDS(3, 5, seed=rng3)
+    
+    # fit with no early stopping 
+    lp, fitted_hmm, posteriors = test_lds.fit(data, num_iters=100, tol=-1)
+    
+    # some simple tests
+    assert not np.any(np.isnan(lp))
+    assert posteriors.expected_states.shape == (3, 100, 3)
+    
+def test_poisson_lds_em_fit():
+    rng1, rng2, rng3, rng4 = jr.split(SEED, 4)
+    true_lds = PoissonLDS(3, 5, seed=rng1)
+    states, data = true_lds.sample(rng2, num_steps=100, num_samples=3)
+    test_lds = PoissonLDS(3, 5, seed=rng3)
+    
+    # fit with no early stopping 
+    lp, fitted_hmm, posteriors = test_lds.fit(data, 
+                                              num_iters=100,
+                                              tol=-1,
+                                              method="laplace_em",
+                                              rng=rng4)
+    
+    # some simple tests
+    assert not np.any(np.isnan(lp))
+    assert posteriors.expected_states.shape == (3, 100, 3)
