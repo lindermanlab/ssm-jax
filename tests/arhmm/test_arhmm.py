818c843025fb4a8912ce87ee9a1cc51354e20b7f
Collin Schlager <schlagercollin@gmail.com>
2021-11-19 14:03:41 -0800

    add sample consistency tests

--- tests/arhmm/test_arhmm.py
+++ tests/arhmm/test_arhmm.py
@@ -25,6 +25,23 @@ def test_gaussian_arhmm_sample():
     assert states.shape == (32, 10)
     assert data.shape == (32, 10, 5)
     
+def test_gaussian_arhmm_sample_is_consistent():
+    # sampled from ARHMM previously 
+    true_states = np.array([[0, 2, 2],
+                            [0, 0, 0]], dtype=np.int32)
+    true_data = np.array([[[ 1.1885295 ,  0.55225945],
+                           [ 1.3390907 ,  0.8323249 ],
+                           [ 2.7013097 ,  1.469697  ]],
+                          [[-1.3038868 , -1.4337387 ],
+                           [-2.3088303 , -0.5458503 ],
+                           [ 0.1273387 , -1.296866  ]]], dtype=np.float32)
+    
+    rng1, rng2 = jr.split(SEED, 2)
+    arhmm = GaussianARHMM(3, 2, 1, seed=rng1)
+    states, data = arhmm.sample(rng2, num_steps=3, num_samples=2)
+    assert np.all(true_states == states)
+    assert np.allclose(true_data, data)
+    
 def test_gaussian_arhmm_em_fit():
     rng1, rng2, rng3 = jr.split(SEED, 3)
 
