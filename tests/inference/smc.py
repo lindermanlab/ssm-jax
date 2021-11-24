3dcab8cad42f032a031343840608f2e4a5487b59
andrew warrington <andrew.warrington@keble.ox.ac.uk>
2021-11-24 15:21:09 -0800

    adding basic quick test for SMC code.

--- tests/inference/smc.py
+++ tests/inference/smc.py
@@ -1,6 +1,4 @@
-"""
-Test code for SMC filtering/smoothing for SSMs.
-"""
+import pytest
 import tensorflow_probability.substrates.jax as tfp
 from jax.interpreters import xla
 from jax import vmap
@@ -14,24 +12,14 @@ from ssm.hmm import GaussianHMM
 from ssm.arhmm import GaussianARHMM
 from ssm.distributions.glm import GaussianLinearRegression
 from ssm.lds import GaussianLDS
-
 from ssm.inference.smc import smc
 
 
-LML_RTOL = 0.2  # Allowable deviation of ML ratio from 1.0.
+SEED = jr.PRNGKey(0)
+
+LML_RTOL = 0.1   # Allowable deviation of ML ratio from 1.0.
 LML_DISCARD = 1  # The number of high and low ratios that can be discarded
 
-NUM_ROUNDS = 10
-NUM_TRIALS = 5
-NUM_TIMESTEPS = 200
-LATENT_DIM = 3
-EMISSIONS_DIM = 10
-NUM_PARTICLES = 5000
-
-NUM_TRIALS_SWEEP = range(1, 202, 100)
-NUM_TIMESTEPS_SWEEP = range(10, 20011, 10000)
-LATENT_DIM_SWEEP = range(2, 13, 2)
-EMISSIONS_DIM_SWEEP = range(2, 13, 2)
 
 
 @pytest.fixture(autouse=True)
@@ -43,7 +31,14 @@ def cleanup():
     xla._xla_callable.cache_clear()
 
 
-def single_test(key, model_family, num_trials, num_timesteps, num_particles, latent_dim, emissions_dim):
+def _single_smc_test(key,
+                     model_family,
+                     num_rounds,
+                     num_trials,
+                     num_timesteps,
+                     num_particles,
+                     latent_dim,
+                     emissions_dim):
 
     # Define the model.
     key, subkey = jr.split(key)
@@ -54,7 +49,7 @@ def single_test(key, model_family, num_trials, num_timesteps, num_particles, lat
     true_states, data = model.sample(key=subkey, num_steps=num_timesteps, num_samples=num_trials)
 
     # Test against EM (which for the LDS is exact.
-    em_posterior = vmap(model.infer_posterior)(data)
+    em_posterior = vmap(model.e_step)(data)
     em_log_marginal_likelihood = model.marginal_likelihood(data, posterior=em_posterior)
 
     # Close over a single SMC sweep.
@@ -63,7 +58,10 @@ def single_test(key, model_family, num_trials, num_timesteps, num_particles, lat
 
     # Run all the sweeps.
     key, subkey = jr.split(key)
-    _, smc_log_marginal_likelihood, _, _ = vmapped(jr.split(subkey, num=NUM_ROUNDS))
+    smc_posteriors = vmapped(jr.split(subkey, num=num_rounds))
+
+    # Extract the log marginal likelihoods.
+    smc_log_marginal_likelihood = smc_posteriors.log_normalizer
 
     # Compute the expected LML using SMC.
     expected_smc_lml = spsp.logsumexp(smc_log_marginal_likelihood, axis=0) - len(smc_log_marginal_likelihood)
@@ -86,15 +84,32 @@ def single_test(key, model_family, num_trials, num_timesteps, num_particles, lat
     return passed
 
 
-def test_smc_lds():
+def test_single_smc_gaussian_lds():
 
-    closed_single_test = lambda _k, _trials, _timesteps, _particles, _latent_dim, _emissions_dim: \
-        single_test(_k, GaussianLDS, _trials, _timesteps, _particles, _latent_dim, _emissions_dim)
+    num_rounds = 5
+    num_trials = 5
+    num_timesteps = 100
+    num_particles = 100
+    latent_dim = 3
+    emissions_dim = 10
 
-    key = jr.PRNGKey(0)
-    assert closed_single_test(key, NUM_TRIALS, NUM_TIMESTEPS, NUM_PARTICLES, LATENT_DIM, EMISSIONS_DIM)
+    model_family = GaussianLDS
+    key, subkey = jr.split(SEED)
+
+    assert _single_smc_test(subkey,
+                            model_family,
+                            num_rounds,
+                            num_trials,
+                            num_timesteps,
+                            num_particles,
+                            latent_dim,
+                            emissions_dim)
 
 
-    p = 0
+@pytest.mark.slow
+def test_multiple_smc_gaussian_lds():
+    pass
 
-test_smc_lds()
+
+if __name__ == '__main__':
+    test_single_smc_gaussian_lds()
