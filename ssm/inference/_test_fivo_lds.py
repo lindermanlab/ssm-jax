minor bug fixes.  need to now establish if the sweeps are of a higher diversity, or it is just lower variance by some other means...

ssm/inference/_test_fivo_lds.py
@@ -18,6 +18,17 @@ import ssm.inference.tilts as tilts
 
 
 def lds_define_test(key, free_parameters, proposal_structure, tilt_structure):
+    """
+
+    Args:
+        key:
+        free_parameters:
+        proposal_structure:
+        tilt_structure:
+
+    Returns:
+
+    """
 
     # Define the true model.
     key, subkey = jr.split(key)
@@ -46,9 +57,13 @@ def lds_define_test(key, free_parameters, proposal_structure, tilt_structure):
 def lds_define_test_model(key, true_model, free_parameters):
     """
 
-    :param subkey:
-    :param true_model:
-    :return:
+    Args:
+        key:
+        true_model:
+        free_parameters:
+
+    Returns:
+
     """
     key, subkey = jr.split(key)
 
@@ -94,6 +109,9 @@ def lds_define_test_model(key, true_model, free_parameters):
 
 
 class LdsTilt(tilts.IndependentGaussianTilt):
+    """
+
+    """
 
     def apply(self, params, inputs):
         """
@@ -132,7 +150,6 @@ class LdsTilt(tilts.IndependentGaussianTilt):
         sds = r_dist.variance().T
 
         # Sweep over the vector and return zeros where appropriate.
-        # TODO - this should be made into a scan or a vmap...
         def _eval(_idx, _mu, _sd, _out):
             _dist = tfd.MultivariateNormalDiag(loc=np.expand_dims(_mu, -1), scale_diag=np.sqrt(np.expand_dims(_sd, -1)))
             return jax.lax.cond(_idx < t,
@@ -141,13 +158,6 @@ class LdsTilt(tilts.IndependentGaussianTilt):
                                 None)
 
         log_r_val = jax.vmap(_eval)(np.arange(means.shape[0]), means, sds, tilt_outputs).sum(axis=0)
-
-        # log_r_val = 0.0
-        # for _mu, _sd, _out in zip(means, sds, tilt_outputs):
-        #     log_r_val = log_r_val + jax.lax.cond(np.any(np.isnan(_out)),
-        #                                          lambda *args: np.zeros(means.shape[1]),
-        #                                          lambda *args: _dist.log_prob(np.asarray([_out])),
-        #                                          None)
 
         return log_r_val
 
@@ -238,49 +248,6 @@ def lds_define_tilt(subkey, model, dataset, tilt_structure):
 
     tilt_params = tilt.init(subkey)
 
-    #
-    # Make the params a dict.
-    # TODO - this is hella messy.
-    # tilt = utils.JaxTuple(tilt)
-    # tilt_params = tuple(tilt_params)
-    # jax.pmap(lambda _p, _k: _p.init(_k))(tilt, jr.split(subkey, n_tilts))
-
-    # # Tilt functions take in (dataset, model, particles, t-1).
-    # dummy_particles = model.initial_distribution().sample(seed=jr.PRNGKey(0), sample_shape=(2,), )
-    # output_lengths = np.arange(len(dataset[0]), 0, -1)
-    # tilt = []
-    # tilt_params = []
-    #
-    # for _t in range(n_tilts):
-    #     _stock_tilt_input = (dataset[-1], model, dummy_particles[0], _t)
-    #
-    #     _dummy_tilt_output = nn_util.vectorize_pytree(dataset[0][-output_lengths[_t]:], )
-    #     _head_mean_fn = nn.Dense(_dummy_tilt_output.shape[0])
-    #     _head_log_var_fn = nn_util.Static(_dummy_tilt_output.shape[0])
-    #
-    #     # Define the tilts themselves.
-    #     _tilt = tilts.IndependentGaussianTilt(n_tilts=1,
-    #                                           tilt_input=_stock_tilt_input,
-    #                                           input_generator=lds_tilt_input_generator,
-    #                                           output_generator=lds_tilt_output_generator,
-    #                                           head_mean_fn=_head_mean_fn,
-    #                                           head_log_var_fn=_head_log_var_fn)
-    #
-    #     subkey, another_subkey = jr.split(subkey)
-    #     _tilt_params = _tilt.init(another_subkey)
-    #
-    #     tilt.append(_tilt)
-    #     tilt_params.append(_tilt_params)
-    #
-    # # Make the params a dict.
-    # # TODO - this is hella messy.
-    #
-    # # tilt = utils.JaxTuple(tilt)
-    #
-    # tilt_params = utils.make_named_tuple({'t' + str(_k): _p for _p, _k in zip(tilt_params, np.arange(10))})
-    # # tilt_params = tuple(tilt_params)
-    # # jax.pmap(lambda _p, _k: _p.init(_k))(tilt, jr.split(subkey, n_tilts))
-
     # Return a function that we can call with just the parameters as an argument to return a new closed proposal.
     rebuild_tilt_fn = tilts.rebuild_tilt(tilt, tilt_structure)
     return tilt, tilt_params, rebuild_tilt_fn
@@ -289,10 +256,14 @@ def lds_define_tilt(subkey, model, dataset, tilt_structure):
 def lds_define_proposal(subkey, model, dataset, proposal_structure):
     """
 
-    :param subkey:
-    :param model:
-    :param dataset:
-    :return:
+    Args:
+        subkey:
+        model:
+        dataset:
+        proposal_structure:
+
+    Returns:
+
     """
 
     if (proposal_structure is None) or (proposal_structure == 'BOOTSTRAP'):
@@ -371,8 +342,11 @@ def lds_get_true_target_marginal(model, data):
 def lds_define_true_model_and_data(key):
     """
 
-    :param key:
-    :return:
+    Args:
+        key:
+
+    Returns:
+
     """
     latent_dim = 1
     emissions_dim = 1
@@ -384,7 +358,7 @@ def lds_define_true_model_and_data(key):
     true_dynamics_weights = np.eye(latent_dim)
     true_emission_weights = np.eye(emissions_dim)
 
-    emission_scale_tril = 0.1 * np.eye(emissions_dim)  # TODO - made observations tighter.
+    emission_scale_tril = 0.25 * np.eye(emissions_dim)  # TODO - made observations tighter.
     # emission_scale_tril = 1.0 * np.eye(emissions_dim)
 
     initial_state_scale_tril = 5.0 * np.eye(latent_dim)
@@ -463,16 +437,19 @@ def lds_do_plot(_param_hist, _loss_hist, _true_loss_em, _true_loss_smc, _true_pa
 
 def lds_do_print(_step, true_model, opt, true_lml, pred_lml, pred_fivo_bound, em_log_marginal_likelihood=None):
     """
-    Do menial print stuff.
-    :param _step:
-    :param pred_lml:
-    :param true_model:
-    :param true_lml:
-    :param opt:
-    :param em_log_marginal_likelihood:
-    :return:
-    """
 
+    Args:
+        _step:
+        true_model:
+        opt:
+        true_lml:
+        pred_lml:
+        pred_fivo_bound:
+        em_log_marginal_likelihood:
+
+    Returns:
+
+    """
     _str = 'Step: {: >5d},  True Neg-LML: {: >8.3f},  Pred Neg-LML: {: >8.3f},  Pred FIVO bound {: >8.3f}'.\
         format(_step, true_lml, pred_lml, pred_fivo_bound)
     if em_log_marginal_likelihood is not None:

ssm/inference/fivo.py
@@ -74,7 +74,7 @@ def do_fivo_sweep(_param_vals,
                                                                _model,
                                                                np.zeros(_dataset.shape[-1], ),
                                                                0,
-                                                               None,
+                                                               _model.initial_distribution(),
                                                                None)
     else:
         initial_distribution = _proposal

ssm/inference/fivo_util.py
@@ -194,30 +194,30 @@ def initial_validation(key, true_model, dataset, true_states, opt, do_fivo_sweep
 
     _plot_single_sweep(true_bpf_posterior[dset_to_plot].filtering_particles,
                        true_states[dset_to_plot],
-                       tag='True BPF Filtering.',
+                       tag='True BPF Filtering (' + str(num_particles) + ' particles).',
                        obs=dataset[dset_to_plot])
     _plot_single_sweep(true_bpf_posterior[dset_to_plot].sample(sample_shape=(num_particles,), seed=jr.PRNGKey(0)),
                        true_states[dset_to_plot],
-                       tag='True BPF Smoothing.',
+                       tag='True BPF Smoothing (' + str(num_particles) + ' particles).',
                        obs=dataset[dset_to_plot])
 
     if init_bpf_posterior is not None:
         _plot_single_sweep(init_bpf_posterior[dset_to_plot].filtering_particles,
                            true_states[dset_to_plot],
-                           tag='Initial BPF Filtering.',
+                           tag='Initial BPF Filtering (' + str(num_particles) + ' particles).',
                            obs=dataset[dset_to_plot])
         _plot_single_sweep(init_bpf_posterior[dset_to_plot].sample(sample_shape=(num_particles,), seed=jr.PRNGKey(0)),
                            true_states[dset_to_plot],
-                           tag='Initial BPF Smoothing.',
+                           tag='Initial BPF Smoothing (' + str(num_particles) + ' particles).',
                            obs=dataset[dset_to_plot])
 
     filt_fig = _plot_single_sweep(init_smc_posterior[dset_to_plot].filtering_particles,
                                   true_states[dset_to_plot],
-                                  tag='Initial SMC Filtering.',
+                                  tag='Initial SMC Filtering (' + str(num_particles) + ' particles).',
                                   obs=dataset[dset_to_plot])
     sweep_fig = _plot_single_sweep(init_smc_posterior[dset_to_plot].sample(sample_shape=(num_particles,), seed=jr.PRNGKey(0)),
                                    true_states[dset_to_plot],
-                                   tag='Initial SMC Smoothing.',
+                                   tag='Initial SMC Smoothing (' + str(num_particles) + ' particles).',
                                    obs=dataset[dset_to_plot])
 
     # Do some print.
@@ -274,7 +274,7 @@ def compare_kls(get_marginals, env, opt, dataset, true_model, rebuild_model_fn, 
     return true_bpf_kls, pred_smc_kls
 
 
-def compare_sweeps(env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop_fn, rebuild_tilt_fn, key, do_fivo_sweep_jitted):
+def compare_sweeps(env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop_fn, rebuild_tilt_fn, key, do_fivo_sweep_jitted, tag='', nrep=10):
     """
 
     Args:
@@ -291,11 +291,13 @@ def compare_sweeps(env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop
     Returns:
 
     """
+    num_particles = env.config.sweep_test_particles
+
     # Do some final validation.
     # Rebuild the initial distribution.
     _prop = rebuild_prop_fn(fivo.get_params_from_opt(opt)[1])
     if _prop is not None:
-        initial_distribution = lambda _dset, _model:  _prop(_dset, _model, np.zeros(dataset.shape[-1], ), 0, None, None)
+        initial_distribution = lambda _dset, _model:  _prop(_dset, _model, np.zeros(dataset.shape[-1], ), 0, _model.initial_distribution(), None)
     else:
         initial_distribution = None
 
@@ -304,7 +306,7 @@ def compare_sweeps(env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop
     final_val_posterior_bpf_true = smc(subkey,
                                        true_model,
                                        dataset,
-                                       num_particles=env.config.sweep_test_particles)
+                                       num_particles=num_particles)
 
     # SMC with tilt.
     key, subkey = jr.split(key)
@@ -314,16 +316,17 @@ def compare_sweeps(env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop
                                        initialization_distribution=initial_distribution,
                                        proposal=rebuild_prop_fn(fivo.get_params_from_opt(opt)[1]),
                                        tilt=rebuild_tilt_fn(fivo.get_params_from_opt(opt)[2]),
-                                       num_particles=env.config.sweep_test_particles)
+                                       num_particles=num_particles)
 
     # CODE for plotting lineages.
-    for _idx in range(10):
+    for _idx in range(nrep):
         fig, ax = plt.subplots(2, 1, sharex=True, sharey=True, squeeze=True, figsize=(8, 8), tight_layout=True)
+        plt.suptitle('Tag: ' + str(tag) + ', ' + str(num_particles) + ' particles.')
 
         for _p in final_val_posterior_bpf_true[_idx].weighted_smoothing_particles:
             ax[0].plot(_p, linewidth=0.1, c='b')
         ax[0].grid(True)
-        ax[0].set_title('BPF in true model.')
+        ax[0].set_title('BPF in true model')
 
         for _p in final_val_posterior_fivo_aux[_idx].weighted_smoothing_particles:
             ax[1].plot(_p, linewidth=0.1, c='b')
@@ -344,6 +347,7 @@ def compare_sweeps(env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop
 
         plt.pause(0.01)
         plt.savefig('./figs/tmp_sweep_{}_{}.pdf'.format(_tag, _idx))
+        plt.close(fig)
 
 
 def final_validation(get_marginals,
@@ -356,7 +360,8 @@ def final_validation(get_marginals,
                      rebuild_tilt_fn, 
                      key, 
                      do_fivo_sweep_jitted, 
-                     GLOBAL_PLOT=True):
+                     GLOBAL_PLOT=True,
+                     tag=''):
     """
 
     Args:
@@ -376,7 +381,7 @@ def final_validation(get_marginals,
     """
 
     # Compare the sweeps.
-    compare_sweeps(get_marginals, env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop_fn, rebuild_tilt_fn, key, do_fivo_sweep_jitted)
+    compare_sweeps(get_marginals, env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop_fn, rebuild_tilt_fn, key, do_fivo_sweep_jitted, tag=tag)
 
     # Compare the KLs.
     true_bpf_kls, pred_smc_kls = compare_kls(get_marginals, env, opt, dataset, true_model, rebuild_model_fn, rebuild_prop_fn,