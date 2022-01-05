62bd318f699abe47208fa967d58de86f3bf203ed
andrew warrington <awarring@stanford.edu>
2022-01-05 13:45:36 -0800

    prep to run biiig boiii

--- ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
+++ ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=gdm-test-1
 #SBATCH --nodes=1 --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=00:29:00
+#SBATCH --time=00:59:59
 #SBATCH --output=./Reports/fivo_%A_%a.out
 #SBATCH --error=./Reports/fivo_%A_%a.err
-#SBATCH --array=0-0
+#SBATCH --array=0-24
 
 module load python/3.9.0
 module load texlive
@@ -14,4 +14,6 @@ echo ${SLURM_ARRAY_TASK_ID}
 echo ${GLOB_TAG}-${EXP_TAG}
 which python3.9
 
-python _test_fivo.py --model ${MODEL} --seed ${SLURM_ARRAY_TASK_ID} --PLOT 0 --use-sgr ${USE_SGR} --proposal-structure ${PROPOSAL_STRUCTURE} --tilt-structure ${TILT_STRUCTURE} --log-group ${GLOB_TAG}-${EXP_TAG}
+pip list
+
+python3.9 _test_fivo.py --model ${MODEL} --seed ${SLURM_ARRAY_TASK_ID} --PLOT 0 --use-sgr ${USE_SGR} --proposal-structure ${PROPOSAL_STRUCTURE} --tilt-structure ${TILT_STRUCTURE} --log-group ${GLOB_TAG}-${EXP_TAG}
