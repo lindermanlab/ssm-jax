dec901a5b579a7f8de74c12fd6dd58e2997aa875
andrew warrington <awarring@stanford.edu>
2022-01-05 12:38:07 -0800

    adding some scripts for launching on sherlock

---
+++ ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=gdm-test-1
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:29:00
+#SBATCH --output=./Reports/fivo_%A_%a.out
+#SBATCH --error=./Reports/fivo_%A_%a.err
+#SBATCH --array=0-2
+
+module load python/3.9.0
+module load texlive
+
+echo ${SLURM_ARRAY_TASK_ID}
+echo ${GLOB_TAG}-${EXP_TAG}
+which python3.9
+
+python _test_fivo.py --model ${MODEL} --seed ${SLURM_ARRAY_TASK_ID} --PLOT 0 --use-sgr ${USE_SGR} --proposal-structure ${PROPOSAL_STRUCTURE} --tilt-structure ${TILT_STRUCTURE} --log-group ${GLOB_TAG}-${EXP_TAG}
---
+++ ssm/inference/cluster_scripts/cluster_launch_multiple.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+glob_tag='gdm-test-v1-0-0'
+model='GDM'
+
+# BPF-SGR
+exp_tag='bpf-sgr'
+use_sgr=1
+proposal_structure='BOOTSTRAP'
+tilt_structure='NONE'
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure} cluster_scripts/_cluster_launch_multiple.sh
+
+# FIVO
+exp_tag='fivo'
+use_sgr=0
+proposal_structure='DIRECT'
+tilt_structure='NONE'
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure} cluster_scripts/_cluster_launch_multiple.sh
+
+# FIVO-AUX
+exp_tag='fivo-aux'
+use_sgr=0
+proposal_structure='DIRECT'
+tilt_structure='DIRECT'
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure} cluster_scripts/_cluster_launch_multiple.sh
+
+# FIVO-AUX-SGR
+exp_tag='fivo-aux-sgr'
+use_sgr=1
+proposal_structure='DIRECT'
+tilt_structure='DIRECT'
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure} cluster_scripts/_cluster_launch_multiple.sh
