fixed the upc thing... by just converting to mean and variance.  this should also be a median and quartile, but for the integer values it doesnt like it.

ssm/inference/_test_fivo.py
@@ -414,19 +414,19 @@ def main():
                           'small_fivo_bound': np.mean(np.asarray(val_fivo_bound)),
                           'small_lml': {'mean': {'em_true': em_log_marginal_likelihood,
                                                  'bpf_true': true_lml,
-                                                 'fivo': np.mean(np.asarray(val_fivo_lml)),},
+                                                 'fivo': np.mean(np.asarray(val_fivo_lml)), },
                                         'variance': {'bpf_true': np.var(np.asarray(true_bpf_lml),),
                                                      'fivo': np.var(np.asarray(val_fivo_lml))}},
                           'expected_kl': {'bpf_true': np.mean(true_bpf_kls),
                                           'fivo': np.mean(pred_smc_kls)},
-                          'expected_upc': {'bpf_true': np.mean(true_bpf_upc),
-                                           'fivo': np.mean(pred_smc_upc), },
-                          'upc': {'true_bpf': true_bpf_upc,
-                                  'fivo': pred_smc_upc, },
+                          'upc': {'mean': {'bpf_true': np.mean(true_bpf_upc, axis=0),
+                                           'fivo': np.mean(pred_smc_upc, axis=0), },
+                                  'variance': {'bpf_true': np.var(true_bpf_upc, axis=0),
+                                               'fivo': np.var(pred_smc_upc, axis=0), },
+                                  },
                           'true_lml': true_lml,
                           }
                 utils.log_to_wandb(to_log, _epoch=_step, USE_WANDB=env.config.use_wandb)
-                print('Done')
 
         # Do some final validation.
         fivo.final_validation(get_marginals,