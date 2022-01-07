043cf084c8a3aa46c9a91630525a74c8a4442906
andrew warrington <awarring@stanford.edu>
2022-01-06 19:57:31 -0800

    run 20 repeats

--- ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
+++ ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=02:59:59
 #SBATCH --output=./Reports/fivo_%A_%a.out
 #SBATCH --error=./Reports/fivo_%A_%a.err
-#SBATCH --array=0-24
+#SBATCH --array=0-19
 
 module load python/3.9.0
 module load texlive
