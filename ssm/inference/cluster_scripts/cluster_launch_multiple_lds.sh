35470a3ae440ea602568fea16d570ac5532ab43b
andrew warrington <awarring@stanford.edu>
2022-01-11 16:47:29 -0800

    update the temper value

--- ssm/inference/cluster_scripts/cluster_launch_multiple_lds.sh
+++ ssm/inference/cluster_scripts/cluster_launch_multiple_lds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-glob_tag='LDS-v1-3-0'
+glob_tag='LDS-v1-4-0'
 model='LDS'
 
 # # BPF-SGR
@@ -32,6 +32,6 @@ exp_tag='fivo-aux-sgr-tempered'
 use_sgr=1
 proposal_structure='RESQ'
 tilt_structure='DIRECT'
-temper=5.0
+temper=4.0
 sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper} cluster_scripts/_cluster_launch_multiple.sh
 
