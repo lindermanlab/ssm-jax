goddamnit, it was because the covariance is defined crazily (just like the emission density was).  once we correct for that it looks okay...

ssm/inference/_test_fivo.py
@@ -67,7 +67,7 @@ def do_config():
     try:
         model = sys.argv[np.where(np.asarray([_a == '--model' for _a in sys.argv]))[0][0] + 1]
     except:
-        model = 'SVM'
+        model = 'LDS'
         print('No model specified, defaulting to: ', model)
 
     if 'LDS' in model:
@@ -87,7 +87,7 @@ def do_config():
     if config['free_parameters'] is None or config['free_parameters'] == '':
         config['free_parameters'] = ()
     else:
-        config['free_parameters'] = tuple(config['free_parameters'].split(','))
+        config['free_parameters'] = tuple(config['free_parameters'].replace(' ', '').split(','))
 
     # Do some type conversions.
     config['use_sgr'] = bool(config['use_sgr'])
@@ -393,7 +393,7 @@ def main():
                 #                                               true_bpf_ess=true_bpf_ess)
 
                 # Do some plotting if we are plotting.
-                if env.config.PLOT and (_step % plot_interval == 0):
+                if env.config.PLOT and ((_step % plot_interval == 0) or (_step == 1)):
 
                     # # Do some plotting.
                     # sweep_fig_filter = _plot_single_sweep(

ssm/inference/_test_fivo_lds.py
@@ -34,27 +34,27 @@ def lds_get_config():
 
     parser.add_argument('--use-sgr', default=1, type=int)  # {0, 1}
 
-    parser.add_argument('--temper', default=0.0, type=float)  # {0.0 to disable,  >0.1 to temper}.
+    parser.add_argument('--temper', default=5.0, type=float)  # {0.0 to disable,  >0.1 to temper}.
 
-    parser.add_argument('--free-parameters', default='dynamics_bias,dynamics_weights', type=str)  # CSV.  # 'dynamics_bias'
+    parser.add_argument('--free-parameters', default='', type=str)  # CSV.  # {'dynamics_bias', 'dynamics_weights'}.
 
     parser.add_argument('--proposal-structure', default='RESQ', type=str)  # {None/'BOOTSTRAP', 'DIRECT', 'RESQ', }
     parser.add_argument('--proposal-type', default='PERSTEP', type=str)  # {'PERSTEP', }.
 
-    parser.add_argument('--tilt-structure', default='DIRECT', type=str)  # {None/'NONE', 'DIRECT'}
-    parser.add_argument('--tilt-type', default='SINGLEWINDOW', type=str)  # {'SINGLEWINDOW', 'PERSTEPWINDOW', 'PERSTEP'}.
+    parser.add_argument('--tilt-structure', default='NONE', type=str)  # {None/'NONE', 'DIRECT'}
+    parser.add_argument('--tilt-type', default='PERSTEPWINDOW', type=str)  # {'SINGLEWINDOW', 'PERSTEPWINDOW', 'PERSTEP'}.
 
     parser.add_argument('--num-particles', default=4, type=int)
-    parser.add_argument('--datasets-per-batch', default=8, type=int)
+    parser.add_argument('--datasets-per-batch', default=32, type=int)
     parser.add_argument('--opt-steps', default=100000, type=int)
 
-    parser.add_argument('--p-lr', default=0.01, type=float)
-    parser.add_argument('--q-lr', default=0.01, type=float)
-    parser.add_argument('--r-lr', default=0.001, type=float)
+    parser.add_argument('--p-lr', default=0.001, type=float)
+    parser.add_argument('--q-lr', default=0.001, type=float)
+    parser.add_argument('--r-lr', default=0.0001, type=float)
 
-    parser.add_argument('--T', default=9, type=int)   # NOTE - This is the number of transitions in the model (index-0).  There are T+1 variables.
-    parser.add_argument('--latent-dim', default=1, type=int)
-    parser.add_argument('--emissions-dim', default=1, type=int)
+    parser.add_argument('--T', default=19, type=int)   # NOTE - This is the number of transitions in the model (index-0).  There are T+1 variables.
+    parser.add_argument('--latent-dim', default=5, type=int)
+    parser.add_argument('--emissions-dim', default=5, type=int)
     parser.add_argument('--num-trials', default=100000, type=int)
 
     parser.add_argument('--dset-to-plot', default=2, type=int)
@@ -206,8 +206,8 @@ def lds_define_proposal(subkey, model, dataset, env):
     dummy_proposal_output = nn_util.vectorize_pytree(np.ones((model.latent_dim,)), )
 
     trunk_fn = None
-    head_mean_fn = nn.Dense(dummy_proposal_output.shape[0])
-    head_log_var_fn = nn_util.Static(dummy_proposal_output.shape[0])
+    head_mean_fn = nn.Dense(dummy_proposal_output.shape[0], kernel_init=lambda *args: nn.initializers.lecun_normal()(*args) * 0.01, )
+    head_log_var_fn = nn_util.Static(dummy_proposal_output.shape[0], bias_init=nn.initializers.zeros, )
 
     # trunk_fn = nn_util.MLP([6, ], output_layer_relu=True)
     # head_mean_fn = nn.Dense(dummy_proposal_output.shape[0])
@@ -291,7 +291,7 @@ def lds_define_true_model_and_data(key, env):
 
     """
 
-    dynamics_scale_tril = None
+    # dynamics_scale_tril = None
     true_dynamics_weights = None
     true_emission_weights = None
     emission_scale_tril = None
@@ -302,14 +302,15 @@ def lds_define_true_model_and_data(key, env):
     num_trials = env.config.num_trials
     T = env.config.T  # NOTE - This is the number of transitions in the model (index-0).  There are T+1 variables.
 
+    # NOTE - Set the dynamics scale here.
+    # This needs to be done for all models because it is defined poorly inside the model.
+    dynamics_scale_tril = 1.0 * np.eye(latent_dim)
+
     # If we are in the 1-D case, then we need to define a boring LDS.
     if latent_dim == 1:
         # Set up the transmission and emission weights to be unity.
         true_dynamics_weights = np.eye(latent_dim)
         true_emission_weights = np.eye(emissions_dim, latent_dim)
-
-        # NOTE - Set the dynamics scale here.
-        dynamics_scale_tril = 1.0 * np.eye(latent_dim)
 
         # NOTE - can make observations tighter here.
         emission_scale_tril = 1.0 * np.eye(emissions_dim)