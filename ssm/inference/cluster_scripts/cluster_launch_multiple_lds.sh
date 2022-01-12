7bd2eb951d8c43981ea362e4d60a742a6ebd16b9
andrew warrington <awarring@stanford.edu>
2022-01-11 17:03:48 -0800

    explicitly set the types

--- ssm/inference/cluster_scripts/cluster_launch_multiple_lds.sh
+++ ssm/inference/cluster_scripts/cluster_launch_multiple_lds.sh
@@ -2,6 +2,8 @@
 
 glob_tag='LDS-v1-4-0'
 model='LDS'
+proposal_type='PERSTEP'
+tilt_type='SINGLEWINDOW'
 
 # # BPF-SGR
 exp_tag='bpf-sgr'
@@ -11,7 +13,7 @@ tilt_structure='NONE'
 temper=0.0
 latent_dim=1
 emission_dim=1
-sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim} cluster_scripts/_cluster_launch_multiple.sh
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},PROPOSAL_TYPE=${proposal_type},TILT_TYPE=${tilt_type} cluster_scripts/_cluster_launch_multiple.sh
 
 # FIVO
 exp_tag='fivo'
@@ -21,7 +23,7 @@ tilt_structure='NONE'
 temper=0.0
 latent_dim=1
 emission_dim=1
-sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim} cluster_scripts/_cluster_launch_multiple.sh
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},PROPOSAL_TYPE=${proposal_type},TILT_TYPE=${tilt_type} cluster_scripts/_cluster_launch_multiple.sh
 
 # FIVO-AUX-SGR
 exp_tag='fivo-aux-sgr'
@@ -31,7 +33,7 @@ tilt_structure='DIRECT'
 temper=0.0
 latent_dim=1
 emission_dim=1
-sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim} cluster_scripts/_cluster_launch_multiple.sh
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},PROPOSAL_TYPE=${proposal_type},TILT_TYPE=${tilt_type} cluster_scripts/_cluster_launch_multiple.sh
 
 # FIVO-AUX-SGR-TEMPERED
 exp_tag='fivo-aux-sgr-tempered'
@@ -41,5 +43,5 @@ tilt_structure='DIRECT'
 temper=4.0
 latent_dim=1
 emission_dim=1
-sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim} cluster_scripts/_cluster_launch_multiple.sh
+sbatch --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},TILT_STRUCTURE=${tilt_structure},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},PROPOSAL_TYPE=${proposal_type},TILT_TYPE=${tilt_type} cluster_scripts/_cluster_launch_multiple.sh
 
