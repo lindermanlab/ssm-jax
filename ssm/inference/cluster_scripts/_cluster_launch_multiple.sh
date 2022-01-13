2d85f171e0cacffc270c9139dc34f6e5f1790bdd
andrew warrington <awarring@stanford.edu>
2022-01-13 13:49:08 -0800

    minor

--- ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
+++ ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=04:59:59
 #SBATCH --output=./Reports/fivo_%A_%a.out
 #SBATCH --error=./Reports/fivo_%A_%a.err
-#SBATCH --array=0-6%3
+#SBATCH --array=0-5%3
 
 module load python/3.9.0
 module load texlive
--- ssm/inference/cluster_scripts/cluster_launch_multiple_lds_large.sh
+++ ssm/inference/cluster_scripts/cluster_launch_multiple_lds_large.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-glob_tag='LDS-large-v1-0-0'
+glob_tag='LDS-large-v2-0-0'
 model='LDS'
 proposal_type='PERSTEP'
 tilt_type='SINGLEWINDOW'
@@ -36,5 +36,5 @@ exp_tag='fivo-aux-sgr-tempered'
 use_sgr=1
 proposal_structure='RESQ'
 tilt_structure='DIRECT'
-temper=4.0
+temper=5.0
 sbatch -J ${glob_tag} --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},PROPOSAL_TYPE=${proposal_type},TILT_STRUCTURE=${tilt_structure},TILT_TYPE=${tilt_type},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim} cluster_scripts/_cluster_launch_multiple.sh
