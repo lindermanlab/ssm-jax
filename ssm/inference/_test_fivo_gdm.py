32fa38434461331973de3319591323926efb29ec
andrew warrington <andrew.warrington@keble.ox.ac.uk>
2022-01-15 13:22:22 -0800

    going to run ALL THE EXPERIMENTS

--- ssm/inference/_test_fivo_gdm.py
+++ ssm/inference/_test_fivo_gdm.py
@@ -40,6 +40,7 @@ def gdm_get_config():
 
     parser.add_argument('--proposal-structure', default='DIRECT', type=str)         # {None/'NONE'/'BOOTSTRAP', 'DIRECT' }
     parser.add_argument('--proposal-type', default='PERSTEP', type=str)             # {'PERSTEP', }.
+    parser.add_argument('--proposal-window-length', default=None, type=int)             # {int, None}.
 
     parser.add_argument('--tilt-structure', default='DIRECT', type=str)             # {None/'NONE', 'DIRECT'}
     parser.add_argument('--tilt-type', default='PERSTEP', type=str)                 # {'PERSTEP'}.
--- ssm/inference/_test_fivo_lds.py
+++ ssm/inference/_test_fivo_lds.py
@@ -40,10 +40,16 @@ def lds_get_config():
 
     parser.add_argument('--proposal-structure', default='RESQ', type=str)  # {None/'NONE'/'BOOTSTRAP', 'DIRECT', 'RESQ', }
     parser.add_argument('--proposal-type', default='PERSTEP_ALLOBS', type=str)  # {PERSTEP_ALLOBS, 'PERSTEP_SINGLEOBS', 'SINGLE_SINGLEOBS', 'PERSTEP_WINDOW', 'SINGLE_WINDOW'}
+    parser.add_argument('--proposal-window-length', default=None, type=int)             # {int, None}.
 
     parser.add_argument('--tilt-structure', default='DIRECT', type=str)  # {None/'NONE', 'DIRECT'}
     parser.add_argument('--tilt-type', default='SINGLE_WINDOW', type=str)  # {'PERSTEP_ALLOBS', 'PERSTEP_WINDOW', 'SINGLE_WINDOW'}.
     parser.add_argument('--tilt-window-length', default=2, type=int)  # {int, None}.
+
+    parser.add_argument('--vi-use-tilt-gradient', default=0, type=int)  # {0, 1}.
+    parser.add_argument('--vi-buffer-length', default=10, type=int)  #
+    parser.add_argument('--vi-minibatch-size', default=16, type=int)  #
+    parser.add_argument('--vi-epochs', default=1, type=int)  #
 
     parser.add_argument('--num-particles', default=8, type=int)
     parser.add_argument('--datasets-per-batch', default=16, type=int)
