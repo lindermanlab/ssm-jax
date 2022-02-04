52832e5dc9c87fbc15cd8d25e04d87a56cfc40eb
andrew warrington <awarring@stanford.edu>
2022-02-04 08:42:55 -0800

    whoops, use filtering

--- ssm/inference/cluster_scripts/cluster_launch_multiple_all.sh
+++ ssm/inference/cluster_scripts/cluster_launch_multiple_all.sh
@@ -1,4 +1,5 @@
-./cluster_scripts/cluster_launch_multiple_gdm.sh
-# ./cluster_scripts/cluster_launch_multiple_lds_large.sh
+# ./cluster_scripts/cluster_launch_multiple_gdm.sh
 ./cluster_scripts/cluster_launch_multiple_lds.sh
 ./cluster_scripts/cluster_launch_multiple_svm.sh
+
+# ./cluster_scripts/cluster_launch_multiple_lds_large.sh
