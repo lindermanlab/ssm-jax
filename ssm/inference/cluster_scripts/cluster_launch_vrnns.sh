cb002f85c41deab7c2014b5fb9152a3781526072
andrew warrington <awarring@stanford.edu>
2022-02-08 10:47:52 -0800

    aligning vrnn scripts

--- ssm/inference/cluster_scripts/cluster_launch_vrnns.sh
+++ ssm/inference/cluster_scripts/cluster_launch_vrnns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 shopt -s expand_aliases
 
-glob_tag='VRNN-JSB-v0-0-0'
+glob_tag='VRNN-JSB-v0-0-1'
 model='VRNN'
 latent_dim=32
 emissions_dim=1
@@ -13,11 +13,11 @@ resamp_crit='ess_criterion'
 
 # launch_cmd () { sbatch -p gpu -G 1 -C GPU_MEM:16GB -t 4:00:00 -J ${glob_tag} --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},PROPOSAL_TYPE=${proposal_type},TILT_STRUCTURE=${tilt_structure},TILT_TYPE=${tilt_type},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},USE_VI=${use_vi},N_PART=${n_part},ENCODER_STRUCT=${enc_struct},RESAMP_CRIT=${resamp_crit} cluster_scripts/_cluster_launch_multiple.sh ; }
 
-launch_cmd () { sbatch --cpus-per-task=1 -J ${glob_tag} --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},PROPOSAL_TYPE=${proposal_type},TILT_STRUCTURE=${tilt_structure},TILT_TYPE=${tilt_type},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},USE_VI=${use_vi},N_PART=${n_part},ENCODER_STRUCT=${enc_struct},RESAMP_CRIT=${resamp_crit} cluster_scripts/_cluster_launch_multiple.sh ; }
+launch_cmd () { sbatch --cpus-per-task=4 --mem=20GB -J ${glob_tag} --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},PROPOSAL_TYPE=${proposal_type},TILT_STRUCTURE=${tilt_structure},TILT_TYPE=${tilt_type},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},USE_VI=${use_vi},N_PART=${n_part},ENCODER_STRUCT=${enc_struct},RESAMP_CRIT=${resamp_crit} cluster_scripts/_cluster_launch_multiple.sh ; }
 
 
 # BPF-SGR
-exp_tag='BPF-SGR'
+exp_tag='bpf-sgr'
 use_sgr=0
 proposal_type='NONE'
 proposal_structure='NONE'
@@ -28,7 +28,7 @@ launch_cmd
 
 
 # FIVO-FILTER
-exp_tag='FIVO'
+exp_tag='fivo'
 use_sgr=0
 proposal_type='VRNN_FILTERING'
 proposal_structure='VRNN_FILTERING_RESQ'
@@ -38,7 +38,7 @@ enc_struct='NONE'
 launch_cmd
 
 # FIVO-FILTER
-exp_tag='FIVO+smooth'
+exp_tag='fivo+smooth'
 use_sgr=0
 proposal_type='VRNN_SMOOTHING'
 proposal_structure='VRNN_SMOOTHING_RESQ'
@@ -49,7 +49,7 @@ launch_cmd
 
 
 # FIVO-AUX-SGR-WINDOW
-exp_tag='FIVO-AUX-SGR-WINDOW'
+exp_tag='fivo-aux-sgr-window'
 use_sgr=1
 proposal_type='VRNN_SMOOTHING'
 proposal_structure='VRNN_SMOOTHING_RESQ'
@@ -59,7 +59,7 @@ enc_struct='BIRNN'
 launch_cmd
 
 # FIVO-AUX-SGR-ENCODED
-exp_tag='FIVO-AUX-SGR-ENCODED'
+exp_tag='fivo-aux-sgr-encoded'
 use_sgr=1
 proposal_type='VRNN_SMOOTHING'
 proposal_structure='VRNN_SMOOTHING_RESQ'
