051cee0ded42bba066b7fa43e54c94339d7765a3
andrew warrington <awarring@stanford.edu>
2022-02-10 14:42:31 -0800

    maybe export helps..

--- ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
+++ ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
@@ -5,32 +5,36 @@
 #SBATCH --time=05:59:59
 #SBATCH --output=./Reports/fivo_%A_%a.out
 #SBATCH --error=./Reports/fivo_%A_%a.err
-#SBATCH --array=0-9%5
+#SBATCH --array=0-1%5
 
 module load python/3.9.0
 module load texlive
 
 echo ${SLURM_ARRAY_TASK_ID}
-echo ${GLOB_TAG}-${EXP_TAG}
-echo ${TEMPER}
+echo ${glob_tag}-${exp_tag}
+echo ${temper}
 which python3.9
 
 pip list
 
-echo "GLOB_TAG:   " $GLOB_TAG
-echo "EXP_TAG:    " $EXP_TAG
-echo "MODEL:      " $MODEL
-echo "USE SGR:    " $USE_SGR
-echo "PROP STRUC: " $PROPOSAL_STRUCTURE
-echo "PROP TYPE:  " $PROPOSAL_TYPE
-echo "TILT STRUC: " $TILT_STRUCTURE
-echo "TILT TYPE:  " $TILT_TYPE
-echo "TEMPER:     " $TEMPER
-echo "LATENT DIM: " $LATENT_DIM
-echo "EM DIM:     " $EMISSIONS_DIM
-echo "USE VI:     " $USE_VI
-echo "N PARTICLE: " $N_PART
-echo "ENC STRUCT: " $ENCODER_STRUCT
-echo "resamp_crit:" $RESAMP_CRIT
+echo "\n"
+echo "HYPERPARAMETERS:"
+echo "\tGLOB_TAG:   " $glob_tag
+echo "\tEXP_TAG:    " $exp_tag
+echo "\tMODEL:      " $model
+echo "\tUSE SGR:    " $use_sgr
+echo "\tPROP STRUC: " $proposal_structure
+echo "\tPROP TYPE:  " $proposal_type
+echo "\tTILT STRUC: " $tilt_structure
+echo "\tTILT TYPE:  " $tilt_type
+echo "\tTEMPER:     " $temper
+echo "\tLATENT DIM: " $latent_dim
+echo "\tEM DIM:     " $emissions_dim
+echo "\tUSE VI:     " $use_vi
+echo "\tN PARTICLE: " $n_part
+echo "\tENC STRUCT: " $enc_struct
+echo "\tTr resamp c:" $train_resamp_crit
+echo "\tEv resamp c:" $eval_resamp_crit
 
-python3.9 _test_fivo.py --model ${MODEL} --seed ${SLURM_ARRAY_TASK_ID} --PLOT 0 --use-sgr ${USE_SGR} --proposal-structure ${PROPOSAL_STRUCTURE} --proposal-type ${PROPOSAL_TYPE} --tilt-structure ${TILT_STRUCTURE} --tilt-type ${TILT_TYPE} --log-group ${GLOB_TAG}-${EXP_TAG} --temper "${TEMPER}" --latent-dim ${LATENT_DIM} --emissions-dim ${EMISSIONS_DIM} --vi-use-tilt-gradient ${USE_VI} --num-particles ${N_PART} --encoder-structure ${ENCODER_STRUCT} --resampling-criterion ${RESAMP_CRIT} --dataset ${DATASET}
+
+python3.9 _test_fivo.py --model ${model} --seed ${SLURM_ARRAY_TASK_ID} --PLOT 0 --use-sgr ${use_sgr} --proposal-structure ${proposal_structure} --proposal-type ${proposal_type} --tilt-structure ${tilt_structure} --tilt-type ${tilt_type} --log-group ${glob_tag}-${exp_tag} --temper "${temper}" --latent-dim ${latent_dim} --emissions-dim ${emissions_dim} --vi-use-tilt-gradient ${use_vi} --num-particles ${n_part} --encoder-structure ${encoder_struct} --train-resampling-criterion ${train_resamp_crit} --eval-resampling-criterion ${eval_resamp_crit} --dataset ${dataset}
--- ssm/inference/cluster_scripts/cluster_launch_multiple_vrnn.sh
+++ ssm/inference/cluster_scripts/cluster_launch_multiple_vrnn.sh
@@ -1,96 +1,157 @@
 #!/bin/bash
 shopt -s expand_aliases
 
-dataset='jsb'
-latent_dim=32
+export dataset='jsb'
+export latent_dim=32
 
-n_part=4
+export n_part_global=4
 
 # These are reasonably constant for VRNN experiments
-glob_tag="VRNN-${dataset}-${n_part}-v1-0-5"
-model='VRNN'
-emissions_dim=1
-temper=0.0
-use_vi=0
-resamp_crit='ess_criterion'
+export glob_tag="VRNN-${dataset}-${n_part}-v1-0-5"
+export model ='VRNN'
+export emissions_dim=1
 
 # launch_cmd () { sbatch -p gpu -G 1 -C GPU_MEM:16GB -t 4:00:00 -J ${glob_tag} --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},PROPOSAL_TYPE=${proposal_type},TILT_STRUCTURE=${tilt_structure},TILT_TYPE=${tilt_type},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},USE_VI=${use_vi},N_PART=${n_part},ENCODER_STRUCT=${enc_struct},RESAMP_CRIT=${resamp_crit},DATASET={dataset} cluster_scripts/_cluster_launch_multiple.sh ; }
 
-launch_cmd () { sbatch --cpus-per-task=4 --mem=20GB -t 9:59:00 -J ${glob_tag} --export=GLOB_TAG=$glob_tag,EXP_TAG=$exp_tag,MODEL=${model},USE_SGR=${use_sgr},PROPOSAL_STRUCTURE=${proposal_structure},PROPOSAL_TYPE=${proposal_type},TILT_STRUCTURE=${tilt_structure},TILT_TYPE=${tilt_type},TEMPER=${temper},LATENT_DIM=${latent_dim},EMISSIONS_DIM=${emissions_dim},USE_VI=${use_vi},N_PART=${n_part},ENCODER_STRUCT=${enc_struct},RESAMP_CRIT=${resamp_crit},DATASET=${dataset} cluster_scripts/_cluster_launch_multiple.sh ; }
+# launch_cmd () { sbatch --cpus-per-task=4 --mem=20GB -t 9:59:00 -J ${glob_tag}-${exp_tag} --export=ALL cluster_scripts/_cluster_launch_multiple.sh ; }
 
+launch_cmd () { sbatch --cpus-per-task=1 -t 0:59:00 -J ${glob_tag}-${exp_tag} --export=ALL cluster_scripts/_cluster_launch_multiple.sh ; }
 
 # BPF-SGR
-exp_tag='bpf-sgr'
-use_sgr=0
-proposal_type='NONE'
-proposal_structure='NONE'
-tilt_type='NONE'
-tilt_structure='NONE'
-enc_struct='NONE'
+export exp_tag='bpf-sgr'
+export use_sgr=0
+export proposal_type='NONE'
+export proposal_structure='NONE'
+export tilt_type='NONE'
+export tilt_structure='NONE'
+export enc_struct='NONE'
+export temper=0.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='ess_criterion'
+export eval_resamp_crit='ess_criterion'
+launch_cmd
+
+# ELBO
+export exp_tag='elbo'
+export use_sgr=0
+export proposal_type='NONE'
+export proposal_structure='NONE'
+export tilt_type='NONE'
+export tilt_structure='NONE'
+export enc_struct='NONE'
+export temper=0.0
+export use_vi=0
+export n_part=1  				# Single particle is used in ELBO
+export train_resamp_crit='never_resample'	# No resampling in ELBO
+export eval_resamp_crit='never_resample'	# No resampling in ELBO
+launch_cmd
+
+# IWAE
+export exp_tag='iwae'
+export use_sgr=0
+export proposal_type='NONE'
+export proposal_structure='NONE'
+export tilt_type='NONE'
+export tilt_structure='NONE'
+export enc_struct='NONE'
+export temper=0.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='never_resample'	# No resampling in IWAE
+export eval_resamp_crit='never_resample'	# No resampling in IWAE
 launch_cmd
 
 
 # FIVO-FILTER
-exp_tag='fivo'
-use_sgr=0
-proposal_type='VRNN_FILTERING'
-proposal_structure='VRNN_FILTERING_RESQ'
-tilt_type='NONE'
-tilt_structure='NONE'
-enc_struct='NONE'
+export exp_tag='fivo'
+export use_sgr=0
+export proposal_type='VRNN_FILTERING'
+export proposal_structure='VRNN_FILTERING_RESQ'
+export tilt_type='NONE'
+export tilt_structure='NONE'
+export enc_struct='NONE'
+export temper=0.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='ess_criterion'
+export eval_resamp_crit='ess_criterion'
 launch_cmd
 
-# FIVO-FILTER
-exp_tag='fivo+smooth'
-use_sgr=0
-proposal_type='VRNN_SMOOTHING'
-proposal_structure='VRNN_SMOOTHING_RESQ'
-tilt_type='NONE'
-tilt_structure='NONE'
-enc_struct='BIRNN'
+# FIVO-SMOOTH
+export exp_tag='fivo+smooth'
+export use_sgr=0
+export proposal_type='VRNN_SMOOTHING'
+export proposal_structure='VRNN_SMOOTHING_RESQ'
+export tilt_type='NONE'
+export tilt_structure='NONE'
+export enc_struct='BIRNN'
+export temper=0.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='ess_criterion'
+export eval_resamp_crit='ess_criterion'
 launch_cmd
 
 
 # FIVO-AUX-SGR-WINDOW
-exp_tag='fivo-aux-sgr-window'
-use_sgr=1
-proposal_type='VRNN_FILTERING'
-proposal_structure='VRNN_FILTERING_RESQ'
-tilt_type='SINGLE_WINDOW'
-tilt_structure='DIRECT'
-enc_struct='NONE'
+export exp_tag='fivo-aux-sgr-window'
+export use_sgr=1
+export proposal_type='VRNN_FILTERING'
+export proposal_structure='VRNN_FILTERING_RESQ'
+export tilt_type='SINGLE_WINDOW'
+export tilt_structure='DIRECT'
+export enc_struct='NONE'
+export temper=0.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='ess_criterion'
+export eval_resamp_crit='ess_criterion'
 launch_cmd
 
 # FIVO-AUX-SGR-ENCODED
-exp_tag='fivo-aux-sgr-encoded'
-use_sgr=1
-proposal_type='VRNN_SMOOTHING'
-proposal_structure='VRNN_SMOOTHING_RESQ'
-tilt_type='ENCODED'
-tilt_structure='DIRECT'
-enc_struct='BIRNN'
+export exp_tag='fivo-aux-sgr-encoded'
+export use_sgr=1
+export proposal_type='VRNN_SMOOTHING'
+export proposal_structure='VRNN_SMOOTHING_RESQ'
+export tilt_type='ENCODED'
+export tilt_structure='DIRECT'
+export enc_struct='BIRNN'
+export temper=0.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='ess_criterion'
+export eval_resamp_crit='ess_criterion'
 launch_cmd
 
 
 # FIVO-AUX-SGR-WINDOW-TEMPER
-exp_tag='fivo-aux-sgr-window-temper'
-use_sgr=1
-proposal_type='VRNN_FILTERING'
-proposal_structure='VRNN_FILTERING_RESQ'
-tilt_type='SINGLE_WINDOW'
-tilt_structure='DIRECT'
-enc_struct='NONE'
-temper=1.0
+export exp_tag='fivo-aux-sgr-window-temper'
+export use_sgr=1
+export proposal_type='VRNN_FILTERING'
+export proposal_structure='VRNN_FILTERING_RESQ'
+export tilt_type='SINGLE_WINDOW'
+export tilt_structure='DIRECT'
+export enc_struct='NONE'
+export temper=1.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='ess_criterion'
+export eval_resamp_crit='ess_criterion'
 launch_cmd
 
 # FIVO-AUX-SGR-ENCODED-TEMPER
-exp_tag='fivo-aux-sgr-encodedtempered'
-use_sgr=1
-proposal_type='VRNN_SMOOTHING'
-proposal_structure='VRNN_SMOOTHING_RESQ'
-tilt_type='SINGLE_WINDOW'
-tilt_structure='DIRECT'
-enc_struct='NONE'
-temper=1.0
+export exp_tag='fivo-aux-sgr-encoded-temper'
+export use_sgr=1
+export proposal_type='VRNN_SMOOTHING'
+export proposal_structure='VRNN_SMOOTHING_RESQ'
+export tilt_type='ENCODED'
+export tilt_structure='DIRECT'
+export enc_struct='BIRNN'
+export temper=1.0
+export use_vi=0
+export n_part=n_part_global
+export train_resamp_crit='ess_criterion'
+export eval_resamp_crit='ess_criterion'
 launch_cmd
 
