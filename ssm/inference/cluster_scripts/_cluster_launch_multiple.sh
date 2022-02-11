d5f2ce1fc6eadc965f2120f93fa9060045ee50ed
andrew warrington <awarring@stanford.edu>
2022-02-10 16:59:15 -0800

    minor (quick-push)

--- ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
+++ ssm/inference/cluster_scripts/_cluster_launch_multiple.sh
@@ -19,22 +19,22 @@ pip list
 
 echo "\n"
 echo "HYPERPARAMETERS:"
-echo "\tGLOB_TAG:   " $glob_tag
-echo "\tEXP_TAG:    " $exp_tag
-echo "\tMODEL:      " $model
-echo "\tUSE SGR:    " $use_sgr
-echo "\tPROP STRUC: " $proposal_structure
-echo "\tPROP TYPE:  " $proposal_type
-echo "\tTILT STRUC: " $tilt_structure
-echo "\tTILT TYPE:  " $tilt_type
-echo "\tTEMPER:     " $temper
-echo "\tLATENT DIM: " $latent_dim
-echo "\tEM DIM:     " $emissions_dim
-echo "\tUSE VI:     " $use_vi
-echo "\tN PARTICLE: " $n_part
-echo "\tENC STRUCT: " $enc_struct
-echo "\tTr resamp c:" $train_resamp_crit
-echo "\tEv resamp c:" $eval_resamp_crit
+echo "	GLOB_TAG:   " $glob_tag
+echo "	EXP_TAG:    " $exp_tag
+echo "	MODEL:      " $model
+echo "	USE SGR:    " $use_sgr
+echo "	PROP STRUC: " $proposal_structure
+echo "	PROP TYPE:  " $proposal_type
+echo "	TILT STRUC: " $tilt_structure
+echo "	TILT TYPE:  " $tilt_type
+echo "	TEMPER:     " $temper
+echo "	LATENT DIM: " $latent_dim
+echo "	EM DIM:     " $emissions_dim
+echo "	USE VI:     " $use_vi
+echo "	N PARTICLE: " $n_part
+echo "	ENC STRUCT: " $enc_struct
+echo "	Tr resamp c:" $train_resamp_crit
+echo "	Ev resamp c:" $eval_resamp_crit
 
 
-python3.9 _test_fivo.py --model ${model} --seed ${SLURM_ARRAY_TASK_ID} --PLOT 0 --use-sgr ${use_sgr} --proposal-structure ${proposal_structure} --proposal-type ${proposal_type} --tilt-structure ${tilt_structure} --tilt-type ${tilt_type} --log-group ${glob_tag}---${exp_tag} --temper "${temper}" --latent-dim ${latent_dim} --emissions-dim ${emissions_dim} --vi-use-tilt-gradient ${use_vi} --num-particles ${n_part} --encoder-structure ${enc_struct} --train-resampling-criterion ${train_resamp_crit} --eval-resampling-criterion ${eval_resamp_crit} --dataset ${dataset}
+python3.9 _test_fivo.py --model ${model} --seed ${SLURM_ARRAY_TASK_ID} --PLOT 0 --use-sgr ${use_sgr} --proposal-structure ${proposal_structure} --proposal-type ${proposal_type} --tilt-structure ${tilt_structure} --tilt-type ${tilt_type} --log-group ${glob_tag}__${exp_tag} --temper "${temper}" --latent-dim ${latent_dim} --emissions-dim ${emissions_dim} --vi-use-tilt-gradient ${use_vi} --num-particles ${n_part} --encoder-structure ${enc_struct} --train-resampling-criterion ${train_resamp_crit} --eval-resampling-criterion ${eval_resamp_crit} --dataset ${dataset}
