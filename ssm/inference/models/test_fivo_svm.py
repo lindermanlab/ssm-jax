084284cbd71526ab2d9f406644b37f997e0ed08d
andrew warrington <andrew.warrington@keble.ox.ac.uk>
2022-02-14 09:48:37 -0800

    going to look at the merge

--- ssm/inference/models/test_fivo_svm.py
+++ ssm/inference/models/test_fivo_svm.py
@@ -458,7 +458,7 @@ def define_test_model(key, true_model, env):
             key, subkey = jr.split(key)
 
             # TODO - This needs to be made model-specific.
-            new_val = {_k: _base + (0.1 * jr.normal(key=subkey, shape=_base.shape))}
+            new_val = {_k: _base + (1.0 * jr.normal(key=subkey, shape=_base.shape))}
 
             default_params = utils.mutate_named_tuple_by_key(default_params, new_val)
 
