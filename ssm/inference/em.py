28d1c8e5db8137dd1eee50efaf674a1aa497ad82
Hyun Dong Lee <hyundonglee1015@gmail.com>
2022-11-17 05:18:59 -0800

    jit update

--- ssm/inference/em.py
+++ ssm/inference/em.py
@@ -11,7 +11,7 @@ from dataclasses import dataclass
 class DummyPosterior:
     expected_states: np.ndarray
 
-#@jit # comment it out to debug or use id_print/id_tap
+@jit # comment it out to debug or use id_print/id_tap
 def update(model, data, fixed_zs, covariates, metadata, test_data):
     if fixed_zs is not None:
         posterior = [DummyPosterior(one_hot(fixed_zs[i], model.num_states)) for i in range(len(fixed_zs))]
