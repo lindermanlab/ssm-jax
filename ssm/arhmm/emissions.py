b0af30fc178416858fc53d08f88eefec455563e7
Hyun Dong Lee <hyundonglee1015@gmail.com>
2023-04-17 23:02:04 -0700

    list data

--- ssm/arhmm/emissions.py
+++ ssm/arhmm/emissions.py
@@ -181,8 +181,8 @@ class AutoregressiveEmissions(Emissions):
             return stats
 
         # vmap over all time series in dataset
-        print(dataset[0])
-        print(posteriors[0].expected_states)
+        print(dataset[0].shape)
+        print(posteriors[0].expected_states.shape)
         stats = [vmap(scan_one)(dataset[i], posteriors[i].expected_states) for i in range(len(dataset))]
         print(stats[0].shape)
         stats = np.concatenate(stats)
